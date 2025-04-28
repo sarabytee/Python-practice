c3d0728fd043a5c07b6a163fa6fd5a57985c998d
Sara Djebbari <sara.djebbari@gwu.edu>
2025-04-27 22:37:26 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,12 @@
 import requests
+import os
 
 BASE_URL = "https://api.exchangeratesapi.io/"
 
-API_KEY = "a30e893e82ca422f4298ead804db0cc7"
+API_KEY = os.getenv("EXCHANGE_API_KEY")
+
+if not API_KEY:
+    raise ValueError("API key not found! Set the EXCHANGE_API_KEY environment variable.")
 
 # method to ask user what currency they want to convert to and from
 def convert_currency():
